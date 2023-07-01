1a8e8f32451d8cfb8628be59f7d0b7027700c52d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 17:29:08 -0500

    links created

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,13 @@ import './Nav.css';
 const Nav = () => {
     return (
         <nav className='nav'>
-
+            {['home', 'notes', 'documents', 'about'].map((item) => (
+                <ul className='nav__links'>
+                    <li className='nav__link'>
+                        <Link href={`#`} key={item}>{item}</Link>
+                    </li>
+                </ul>
+            ))}
         </nav>
     )
 }

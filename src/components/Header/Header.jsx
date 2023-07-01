511d0392575a82c2b92d4f69fa1e2e469e9cbbaa
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 17:55:36 -0500

    title and description added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <section className='header'>
+            <h1 className='header__title'>Welcome to my Second Brain</h1>
+            <p className='header__description'>Embark on a journey through the depths of my digital mind. Here you'll find a curated collection of notes, insights, and resources amassed through my odyssey in software development. A treasure trove for the curious and passionate, this space is where knowledge transforms and ideas take flight.</p>
+        </section>
     )
 }
 

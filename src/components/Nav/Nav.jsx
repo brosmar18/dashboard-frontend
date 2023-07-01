622c8b612bb39c4adf8543aedc2df104615c2009
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 17:30:49 -0500

    map updated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,13 +7,15 @@ import './Nav.css';
 const Nav = () => {
     return (
         <nav className='nav'>
-            {['home', 'notes', 'documents', 'about'].map((item) => (
-                <ul className='nav__links'>
-                    <li className='nav__link'>
-                        <Link href={`#`} key={item}>{item}</Link>
+            <ul className='nav__links'>
+                {['home', 'notes', 'documents', 'about'].map((item) => (
+                    <li className='nav__link' key={item}>
+                        <Link href={`#`}>
+                            {item}
+                        </Link>
                     </li>
-                </ul>
-            ))}
+                ))}
+            </ul>
         </nav>
     )
 }

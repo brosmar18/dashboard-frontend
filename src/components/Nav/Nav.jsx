ee5acb999871a25058b3652fa5c7cf9a25ff3cf8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 17:39:20 -0500

    logo image tag created

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,6 +7,7 @@ import './Nav.css';
 const Nav = () => {
     return (
         <nav className='nav'>
+            <img className='nav__logo' src='#' alt='logo' />
             <ul className='nav__links'>
                 {['home', 'notes', 'documents', 'about'].map((item) => (
                     <li className='nav__link' key={item}>

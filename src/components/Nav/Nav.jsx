070664b69cf90ea7140cbc8c18c9bff7ad5da93a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 18:54:53 -0500

    Bootstrap navbar created

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import {images} from '../../assets';
+import { images } from '../../assets';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,9 +13,32 @@ import './Nav.css';
 
 const NavBar = () => {
     return (
-       <Navbar expand="lg" className='bg-body-tertiary'>
-
-       </Navbar>
+        <Navbar expand="lg" className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand href='#'>
+                    <img src={images.logo} alt='logo' />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <NavDropdown title="Notes" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#">Documents</Nav.Link>
+                        <Nav.Link href="#">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 

09ac9f094401d03d09320d1515ad453d284b5f96
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 19:13:46 -0500

    Bootstrap implemented in Header content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,29 @@
+'use client';
+
 import React from 'react';
 import './Header.css';
-import {images} from '../../assets';
+import { images } from '../../assets';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 const Header = () => {
     return (
-        <section className='header'>
-            <h1 className='header__title'>Welcome to my Second Brain</h1>
-            <p className='header__description'>Embark on a journey through the depths of my digital mind. Here you'll find a curated collection of notes, insights, and resources amassed through my odyssey in software development. A treasure trove for the curious and passionate, this space is where knowledge transforms and ideas take flight.</p>
-            <img className='header__image' src={images.header_img} alt='Neuron Network' />
-        </section>
+        <Container>
+            <row>
+                <Row>
+                    <Col>
+                        <h1 className='header__title'>Welcome to My Second Brain</h1>
+                        <p className='header__description'>Embark on a journey through the depths of my digital mind. Here you'll find a curated collection of notes, insights, and resources amassed through my odyssey in software development. A treasure trove for the curious and passionate, this space is where knowledge transforms and ideas take flight.</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img className='header__image' src={images.header_img} alt='Neuron Network' />
+                    </Col>
+                </Row>
+            </row>
+        </Container>
     )
 }
 

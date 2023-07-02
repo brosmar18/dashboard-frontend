664e4b17f1b689c688013398ec52e8c6a49747db
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 19:30:47 -0500

    Styles updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,26 +5,23 @@ import './Header.css';
 import { images } from '../../assets';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
 
 const Header = () => {
     return (
-        <Container>
-            <row>
-                <Row>
-                    <Col>
-                        <h1 className='header__title'>Welcome to My Second Brain</h1>
-                        <p className='header__description'>Embark on a journey through the depths of my digital mind. Here you'll find a curated collection of notes, insights, and resources amassed through my odyssey in software development. A treasure trove for the curious and passionate, this space is where knowledge transforms and ideas take flight.</p>
+        <header className='header'>
+            <Container>
+                <Row className="align-items-center justify-content-center" style={{ height: '70vh' }}>
+                    <Col md={6} className="text-center">
+                        <h1 className='header__title'>My Second Brain</h1>
+                        <p className='header__description'>
+                            Embark on a journey through the depths of my digital mind. Here you'll find a curated collection of notes, insights, and resources amassed through my odyssey in software development. A treasure trove for the curious and passionate, this space is where knowledge transforms and ideas take flight.
+                        </p>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <img className='header__image' src={images.header_img} alt='Neuron Network' />
-                    </Col>
-                </Row>
-            </row>
-        </Container>
-    )
-}
+            </Container>
+        </header>
+    );
+};
 
 export default Header;

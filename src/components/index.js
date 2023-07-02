ca0d0fb60346f08a2afe961ac863e71c73e37a49
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 22:03:59 -0500

    Pages routes updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export {default as NavBar} from './Nav/Nav';
+export {default as NavBar} from './Nav/NavBar';
 export {default as Header} from './Header/Header';
 export {default as NotesMenu} from './NotesMenu/NotesMenu';
 export {default as Footer} from './Footer/Footer';

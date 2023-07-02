90db529d2cec5fdcbd0a8761d68f857d23c773a2
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 19:44:41 -0500

    Bootstrap imported, card built started

--- src/components/NotesMenu/NotesMenu.jsx
+++ src/components/NotesMenu/NotesMenu.jsx
@@ -1,9 +1,26 @@
+'use client';
 import React from 'react';
 import './NotesMenu.css';
+import {images} from '../../assets';
+import {Button, Card, Container, Row, Col} from 'react-bootstrap';
 
 const NotesMenu = () => {
     return (
-        <div>Notes Menu</div>
+        <section className='notes'>
+            <h2 className='notes__title'>My Notes</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card style={{width: '18rem'}}>
+                            <Card.Img variant='top' src={images.logo} alt='logo' />
+                            <Card.Body>
+                                <Card.Title>React.js</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 

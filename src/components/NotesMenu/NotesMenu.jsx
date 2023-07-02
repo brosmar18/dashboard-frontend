fc7e08790d92f5aacef6ccce6a9675d6f7c84f64
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 20:28:53 -0500

    .fetch added

--- src/components/NotesMenu/NotesMenu.jsx
+++ src/components/NotesMenu/NotesMenu.jsx
@@ -1,10 +1,20 @@
 'use client';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './NotesMenu.css';
 import {images} from '../../assets';
 import {Button, Card, Container, Row, Col} from 'react-bootstrap';
+import client, { urlFor } from '../../client/client';
 
 const NotesMenu = () => {
+    const [categories, setCateegories] = useState([]);
+
+    useEffect(() => {
+        const query = `*[_type == "notesCategory"]`;
+
+        client.fetch(query)
+        .then((data) => setCateegories(data));
+    }, [])
+
     return (
         <section className='notes'>
             <h2 className='notes__title'>My Notes</h2>

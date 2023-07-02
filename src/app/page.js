6594e4ddc0e6510778eb53d9f114994029833316
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-01 19:05:21 -0500

    bootstrap styles updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from '../styles/page.module.css'
-import {Nav, Header, NotesMenu, Footer} from '../components';
+import {Nav, Header, NotesMenu, Footer, NavBar} from '../components';
 
 export default function Home() {
   return (
    <div className='page'>
+    <NavBar />
     <Header />
     <NotesMenu />
     <Footer />
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
 import { images } from '../../assets';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,30 +9,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import './Nav.css';
 
-
 const NavBar = () => {
     return (
-        <Navbar expand="lg" className='bg-body-tertiary'>
-            <Container>
+        <Navbar expand="lg" className='bg-body-tertiary custom-navbar'>
+            <Container className="nav-container">
                 <Navbar.Brand href='#'>
                     <img className='logo' src={images.logo} alt='logo' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
+                <Navbar.Collapse id='basic-navbar-nav' className='justify-content-center'>
+                    <Nav>
                         <Nav.Link href="#home">Home</Nav.Link>
                         <NavDropdown title="Notes" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Html & Css</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.2">
-                                Javascript
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Javascript</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.3">React.js</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Next.js
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Next.js</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link href="#">Documents</Nav.Link>
                         <Nav.Link href="#">About</Nav.Link>

9c5e3882c3460d815a652de9b919d176ae34cf6a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-02 10:44:32 -0500

    hooks added

--- src/app/JavaScript/page.js
+++ src/app/JavaScript/page.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import {NavBar} from '../../components';
+'use client';
+
+import React, {useState, useEffect} from 'react';
+import { NavBar } from '../../components';
 
 const Javascript = () => {
+    const {file, setFile} = useState([]);
+
+    useEffect(() => {
+        const query = `*[_type == "notesFile"]`;
+        
+        client.fetch(query)
+        .then((data) => setFile(data));
+    }, [])
+
+    
     return (
-        <div>
-            <NavBar />
-            <h1>JavaScript Page</h1>
-        </div>
+        <>
+            <section>
+                <NavBar />
+                <h1>JavaScript Page</h1>
+                <p>This is the description of the page.</p>
+            </section>
+            <section>
+                
+            </section>
+
+        </>
     )
 }
 
